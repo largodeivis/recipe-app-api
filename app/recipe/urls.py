0f6ee2c688b34ef39613d8097489984c4e3f6ae0
Deivis Largo <dlargo67@gmail.com>
2021-09-27 16:19:07 -0500

    Added list recipe feature

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -11,6 +11,7 @@ actions in our viewset. ie:
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 app_name = 'recipe'
 
